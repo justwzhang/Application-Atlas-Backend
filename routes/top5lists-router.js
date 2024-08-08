1f98fa57ce3dccfd99e5f6506f1a84bcd23c07a9
justin zhang <justin.zhang.1@stonybrook.edu>
2024-08-07 23:08:19 -0400

    Init from old repo

---
+++ routes/top5lists-router.js
@@ -0,0 +1,27 @@
+const auth = require('../auth')
+const express = require('express')
+const Top5ListController = require('../controllers/top5list-controller')
+const CommunityListController = require('../controllers/communityList-controller')
+const UserController = require('../controllers/user-controller')
+const router = express.Router()
+
+router.post('/top5list', auth.verify, Top5ListController.createTop5List)
+router.put('/top5list/:id', auth.verify, Top5ListController.updateTop5List)
+router.delete('/top5list/:id', auth.verify, Top5ListController.deleteTop5List)
+router.get('/top5list/:id', auth.verify, Top5ListController.getTop5ListById)
+router.get('/top5lists', auth.verify, Top5ListController.getTop5Lists)
+router.get('/top5listpairs', auth.verify, Top5ListController.getTop5ListPairs)
+
+router.post('/register', UserController.registerUser)
+router.get('/loggedIn', UserController.getLoggedIn)
+router.get('/logout', UserController.logoutUser)
+router.post('/login', UserController.logInUser)
+
+router.post('/communitylist', auth.verify, CommunityListController.createCommunityList)
+router.put('/communitylist/:id', auth.verify, CommunityListController.updateCommunityList)
+router.delete('/communitylist/:id', auth.verify, CommunityListController.deleteCommunityList)
+router.get('/communitylist/:id', auth.verify, CommunityListController.getCommunityListById)
+router.get('/communitylists', auth.verify, CommunityListController.getCommunityLists)
+router.get('/communitylistpairs', auth.verify, CommunityListController.getCommunityListPairs)
+
+module.exports = router
